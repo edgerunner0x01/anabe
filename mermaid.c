27791fb3cca46164895f73eabac96e3643024593
edgerunner0x01 <147637952+edgerunner0x01@users.noreply.github.com>
2026-08-17 11:58:45 +0100

    i suck at this

--- mermaid.c
+++ mermaid.c
@@ -1,25 +1,32 @@
 #include <stdio.h>
 
-const int L = 3;
-const int C = 3;
+int memVis(int x){
+    for (int i = 0; i < x; i++){
+        int num = createMemAdr(i);
+        printf("%p : %i\n", &num, *&num);
+    }
+    return 0; 
+}
 
 
 int main(){
+    int sector;
+    printf("Enter Sector of memory to visualize: ");
+    scanf("%i", &sector);
 
-    int nums[L][C];
-    for (int i = 0; i < L; i++){
-        for (int j = 0; j < C; j++){
-            nums[i][j] = i*j; 
-        }
-    }
+    memVis(sector)
+    return 0;
+}
 
 
-    for (int i = 0; i < L; i++){
-        for (int j = 0; j < C; j++){
-            printf("%i ",nums[i][j]); 
-        }
-        printf("\n");
-    }
-
+/*
+int main(){
+    int num = 10 ; 
+    printf("%p : %i\n", &num, num);
+    printf("%i\n", *&num);
+//    int *pNum = num ; 
+//    printf("%i\n", num);
+    
     return 0; 
 }
+*/
